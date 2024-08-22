e56a8ee70fae77662f9c779855650a5c4af78ed0
mikeberdugo <manuel.david.13.b@gmail.com>
2024-08-22 07:39:57 -0500

    .

--- roomiebudget/settings.py
+++ roomiebudget/settings.py
@@ -15,9 +15,7 @@ import os
 from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent.parent
-print('------')
-print(BASE_DIR)
-print('------')
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
